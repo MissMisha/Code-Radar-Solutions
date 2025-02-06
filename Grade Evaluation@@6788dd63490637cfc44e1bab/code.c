d2a3525c52af3880d446cb8031bc60c80c151334
Misha Bansal <mishabansal26@gmail.com>
2025-02-06 17:33:52 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -10,21 +10,27 @@ int main() {
     switch(grade){
         case 'A':
         printf("Excellent");
+        break;
 
         case 'B':
         printf("Good");
+        break;
 
         case 'C':
         printf("Average");
+        break;
 
         case 'D':
         printf("Below Average");
+        break;
 
         case 'F':
         printf("Fail");
+        break;
 
         default:
         printf("Invalid grade");
+    
     }
    
     return 0;
