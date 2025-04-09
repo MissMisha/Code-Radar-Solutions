6ec5dc66c73959577303e30163569779ad5eaaf0
Misha Bansal <mishabansal26@gmail.com>
2025-04-09 18:18:58 +0530

    Solution to Replace Each Element with the Greatest Element on its Right

--- Replace Each Element with the Greatest Element on its Right@@676c02002dbc8c3e7a280b84/code.c
+++ Replace Each Element with the Greatest Element on its Right@@676c02002dbc8c3e7a280b84/code.c
@@ -4,10 +4,11 @@
 int main(){
     int n;
     scanf("%d",&n);
+    int arr[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    int arr[n];
+    
     for(int i=0;i<n;i++){
             int max=0;
             for(int j=i+1;j<n;j++){
